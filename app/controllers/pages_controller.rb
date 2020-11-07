9230dd02284a6c405bd0983720d3f1d254b5abed
Tsuchida-hikaru <70635034+Tsuchida-hikaru@users.noreply.github.com>
2020-11-07 17:38:48 +0900

    cardsコントローラーを生成
    他、pagesコントローラーにトップページへのリダイレクト処理を記述

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
   def show
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end

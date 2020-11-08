feb27aa8c0ba8c170139d6361982f045a5e667a2
Tsuchida-hikaru <70635034+Tsuchida-hikaru@users.noreply.github.com>
2020-11-08 19:35:14 +0900

    ユーザー情報編集機能の実装
    -deviseのeditを編集
    -上記に伴い、usersコントローラーの余分なルーティングとアクションを削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :publish_setting])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :publish_setting])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,9 @@ class UsersController < ApplicationController
   def show
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   private
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permt(:name, :email)
   end
 end

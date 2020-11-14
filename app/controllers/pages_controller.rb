fb6175747017bad0d7c315d9958b11a9b17849f1
Tsuchida-hikaru <70635034+Tsuchida-hikaru@users.noreply.github.com>
2020-11-14 21:25:37 +0900

    pagesコントローラーを削除

--- app/controllers/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-  before_action :move_to_index, except: [:index]
-
-  def index
-    @cards = Card.includes(:user)
-    @card = Card.new
-  end
-end

ed7647d12221b1f27e4d5bd34258a3c666b3c85b
Tsuchida-hikaru <70635034+Tsuchida-hikaru@users.noreply.github.com>
2020-11-14 12:55:28 +0900

    トップページへのリダイレクト処理を編集
    - pagesコントローラーからapplicationコントローラーに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :move_to_index
 
   def after_sign_in_path_for(resource)
     user_path(resource.id)
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :publish_setting])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :publish_setting])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  before_action :move_to_index, except: [:index, :show]
+  skip_before_action :move_to_index, only: :index
 
   def index
     @cards = Card.includes(:user)
     @card = Card.new
   end
-
-  private
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-  
 end

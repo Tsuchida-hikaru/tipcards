f4360b8d767664e50977cef613024107063e7c95
Tsuchida-hikaru <70635034+Tsuchida-hikaru@users.noreply.github.com>
2020-11-14 12:57:26 +0900

    カード投稿機能の実装
    - cardsコントローラーに投稿アクションを実装
    - cardモデルにバリデーションを設置
    - トップページのビューを編集（font-awesomeのバグが発生中）

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,15 @@
 class CardsController < ApplicationController
   def create
+    card = Card.create(card_params)
+    redirect_to root_path if card.save
+  end
 
+  def edit
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :text, :image, :publish_setting).merge(user_id: current_user.id)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,10 @@ class Card < ApplicationRecord
   belongs_to :user
   has_many :users_tags_relations
   has_many :tags, through: :users_tags_relations
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :title, length: { maximum: 30}
+    validates :text
+  end
 end

af28ccb02a7ae88b2d2b90bc798891fc6bad3139
Tsuchida-hikaru <tsuchidahikaru1220@gmail.com>
2020-12-06 23:16:03 +0900

    ユーザーページ修飾１
    - 自身の投稿したカードが一覧表示されるよう修正（ページネーションも追加）
    - フッターの表示位置をコンテンツ高が足りても足りなくてもページ下部に表示されるよう調整
    - フラッシュメッセージ表示部をmain-wrapperの外に移動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @cards = Card.includes(:user).where(user_id: current_user.id).order(updated_at: :DESC)
+    @cards = Card.includes(:user).where(user_id: current_user.id).order(updated_at: :DESC).page(params[:page]).per(5)
   end
 end

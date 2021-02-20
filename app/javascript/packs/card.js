3144d6411f6b1b3878cb2979e6151e94afe88560
Tsuchida-hikaru <tsuchidahikaru1220@gmail.com>
2021-02-20 23:51:49 +0900

    カード開閉機能の実装３
    - card.jsの実装完成
    - カード開閉に関わるSCSSの修飾をこれから設定する予定

--- app/javascript/packs/card.js
+++ app/javascript/packs/card.js
@@ -1,25 +1,32 @@
-// (function (window,document) {
-//     var toggleCardTitle = document.getElementsByClassName('card-title');
-//     var toggleCard = Array.from(document.getElementsByClassName('card-content'));
-//     var clientH;
-//     init();
-//     function init() {
-//         toggleCard.style.height = 'auto';
-//         clientH = toggleCard.clientHeight;
-//         toggleCard.style.height = '0px';
-//         toggleCardTitle.addEventListener('click', function(){clickToggle();}, false);
-//     }
-//     function clickToggle() {
-//         var lastH = toggleCard.style.height;
-//         toggleCard.style.height = (lastH == '' || lastH == '0px')? clientH + 'px' : '0px';
-//     }
-// })(window,document);
 document.addEventListener('DOMContentLoaded', () => {
-    const accordionTrigger = document.querySelectorAll('.card-content');
-    for(let i = 0; i < accordionTrigger.length; i++) {
-        if(accordionTrigger[i].classList.contains('card-title')) {
-            accordionTrigger[i].nextElementSibling.style.height = accordionTrigger[i].nextElementSibling.scrollheight + 'px';
-            
+    // トリガークラスを持つ要素を取得
+    const accordionTrigger = document.querySelectorAll('.card-title');
+    for (let i = 0; i < accordionTrigger.length; i++) {
+        // トリガーが付いている要素を展開したときの高さを付加
+        if (accordionTrigger[i].classList.contains('icon-opened')) {
+            // scrollHeightプロパティでpaddingを含む表示されていない要素の高さを取得
+            accordionTrigger[i].nextElementSibling.style.height = accordionTrigger[i].nextElementSibling.scrollHeight + 'px';
         }
+        // トリガーを押したときのアクション
+        accordionTrigger[i].addEventListener('click', (e) => {
+           // クリックされた要素を取得
+           let currentElement = e.currentTarget;
+           // 同じ親要素を持つ隣接した次の要素（展開対象の要素）を取得
+           let accordionTarget = currentElement.nextElementSibling;
+
+           if (accordionTarget.style.height) {
+               // トリガーのクラスを削除
+               currentElement.classList.remove('icon-opened');
+               accordionTarget.style.color = 'white'
+               accordionTarget.style.height = null;
+           } else {
+               // トリガーのクラスを付加
+               currentElement.classList.add('icon-opened');
+               accordionTarget.style.color = 'black'
+               // scrollHeightプロパティでpaddingを含む表示されていない要素の高さを取得
+               accordionTarget.style.padding = '1rem 0px'
+               accordionTarget.style.height = accordionTarget.scrollHeight + 'px';
+           }
+        });
     }
 });

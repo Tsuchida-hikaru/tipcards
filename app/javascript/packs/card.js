01dc09e5a671ac31eb8887f53be1c2701b5e5e12
Tsuchida-hikaru <tsuchidahikaru1220@gmail.com>
2021-02-20 22:40:27 +0900

    カード開閉機能実装２
    - javascriptの編集中

--- app/javascript/packs/card.js
+++ app/javascript/packs/card.js
@@ -1,3 +1,25 @@
-(function (window,document) {
-    let toggleCardTitle = document.getElements('')
-})(window,document);
+// (function (window,document) {
+//     var toggleCardTitle = document.getElementsByClassName('card-title');
+//     var toggleCard = Array.from(document.getElementsByClassName('card-content'));
+//     var clientH;
+//     init();
+//     function init() {
+//         toggleCard.style.height = 'auto';
+//         clientH = toggleCard.clientHeight;
+//         toggleCard.style.height = '0px';
+//         toggleCardTitle.addEventListener('click', function(){clickToggle();}, false);
+//     }
+//     function clickToggle() {
+//         var lastH = toggleCard.style.height;
+//         toggleCard.style.height = (lastH == '' || lastH == '0px')? clientH + 'px' : '0px';
+//     }
+// })(window,document);
+document.addEventListener('DOMContentLoaded', () => {
+    const accordionTrigger = document.querySelectorAll('.card-content');
+    for(let i = 0; i < accordionTrigger.length; i++) {
+        if(accordionTrigger[i].classList.contains('card-title')) {
+            accordionTrigger[i].nextElementSibling.style.height = accordionTrigger[i].nextElementSibling.scrollheight + 'px';
+            
+        }
+    }
+});
